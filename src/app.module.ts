820c924bfa2798a7d5ebad858aca2649fbab8e31
Brandon Loveall <bloveall02@gmail.com>
2026-03-25 12:41:31 -0600

    nestjs and graphql set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { RootController } from './root.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: process.cwd() + 'src/schema.gql',
+      autoSchemaFile: process.cwd() + '/src/schema.gql',
       sortSchema: true,
     }),
+    UsersModule,
   ],
   controllers: [RootController]
 })
