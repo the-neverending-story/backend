727bc597cdbac07c47e18ac73c35b02ac68ff14d
Brandon Loveall <bloveall02@gmail.com>
2026-04-03 17:50:50 -0600

    testing db connection, will move it to an env variable after its done and change password

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:4200", "http://www.worldofteramir.com"],
+    origin: ["http://localhost:4200", "http://worldofteramir.com/"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   });
