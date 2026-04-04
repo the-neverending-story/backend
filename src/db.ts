0fcfccf78c5f9b5173180c340232ebb611a0c89f
Brandon Loveall <bloveall02@gmail.com>
2026-04-03 20:11:32 -0600

    fixing db string

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import postgres from "postgres";
 
-const pgdb = postgres('postgresql://postgres:Pzc6CjQtUcn04fqpphl9XqPvALGrWwHR@tns-db-cluster.cl44isaiiamm.us-west-2.rds.amazonaws.com:5432/theneverendingstory'); // will use psql environment variables
+const pgdb = postgres('postgresql://postgres:Pzc6CjQtUcn04fqpphl9XqPvALGrWwHR@tns-db-cluster.cl44isaiiamm.us-west-2.rds.amazonaws.com:5432/theneverendingstory?sslmode=verify-full&sslrootcert=./global-bundle.pem'); // will use psql environment variables
 
 export default pgdb;
