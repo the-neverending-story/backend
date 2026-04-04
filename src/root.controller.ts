ef04c31879cf72dd52e32b54f25584d8b6914c7e
Brandon Loveall <bloveall02@gmail.com>
2026-04-04 02:11:49 -0600

    ugh

--- src/root.controller.ts
+++ src/root.controller.ts
@@ -2,8 +2,9 @@
 require("dotenv").config();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-member-access
 require("child_process").exec(
-  `DATABASE_URL=postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_DATABASE${process.env.NODE_ENV === "production" ? "?sslmode=verify-full&sslrootcert=./us-west-2-bundle.pem" : null} npm run migrate up`,
+  `DATABASE_URL=postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}${process.env.NODE_ENV === "production" ? "?sslmode=verify-full&sslrootcert=./us-west-2-bundle.pem" : ''} npm run migrate up`,
   (err, stdout, stderr) => {
+    console.log(`string: DATABASE_URL=postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}${process.env.NODE_ENV === "production" ? "?sslmode=verify-full&sslrootcert=./us-west-2-bundle.pem" : ''}`)
     if (err) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       console.log(err.message);
