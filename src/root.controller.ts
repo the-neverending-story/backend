92573d72015a633819dc2a57d4a9870a256ca8f2
Brandon Loveall <bloveall02@gmail.com>
2026-04-04 02:03:33 -0600

    maybe this works? git status :

--- src/root.controller.ts
+++ src/root.controller.ts
@@ -1,19 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports
 require("dotenv").config();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-member-access
-require("child_process").exec("DATABASE_URL=postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_DATABASE npm run migrate up", (err, stdout, stderr) => {
-  console.log("migration successful");
-  if (err) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    console.log(err.message);
-    return;
-  }
-  if (stderr) {
-    console.log(stderr);
-    return;
-  }
-  console.log(stdout);
-});
+require("child_process").exec(
+  `DATABASE_URL=postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_DATABASE${process.env.NODE_ENV === "production" ? "?sslmode=verify-full&sslrootcert=./us-west-2-bundle.pem" : null} npm run migrate up`,
+  (err, stdout, stderr) => {
+    if (err) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      console.log(err.message);
+      return;
+    }
+    if (stderr) {
+      console.log(stderr);
+      return;
+    }
+    console.log(stdout);
+  },
+);
 
 import { Controller, Get, HttpCode } from "@nestjs/common";
 
