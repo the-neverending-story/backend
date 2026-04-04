d72343e19a8fef0afa698072faf7ab866bec9f47
Brandon Loveall <bloveall02@gmail.com>
2026-04-03 20:29:31 -0600

    trying something different

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,14 @@
 import postgres from "postgres";
 
-const pgdb = postgres('postgresql://postgres:Pzc6CjQtUcn04fqpphl9XqPvALGrWwHR@tns-db-cluster.cl44isaiiamm.us-west-2.rds.amazonaws.com:5432/theneverendingstory?sslmode=verify-full&sslrootcert=./global-bundle.pem'); // will use psql environment variables
+const pgdb = postgres(
+    {
+    password: 'Pzc6CjQtUcn04fqpphl9XqPvALGrWwHR',
+    host: 'tns-db-cluster.cl44isaiiamm.us-west-2.rds.amazonaws.com',
+    port: 5432,
+    database: 'theneverendingstory',
+    user: 'postgres',
+    ssl: { rejectUnauthorized: false, ca: require('fs').readFileSync('./us-west-2-bundle.pem').toString() }
+}
+); // will use psql environment variables
 
 export default pgdb;
