ceda94666a07e7e1c59573070bb5a7881738322e
Brandon Loveall <bloveall02@gmail.com>
2026-04-04 15:07:39 -0600

    migrations should now run when pushing

--- src/root.controller.ts
+++ src/root.controller.ts
@@ -1,22 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports
 require("dotenv").config();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-member-access
-require("child_process").exec(
-  `DATABASE_URL=postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}${process.env.NODE_ENV === "production" ? "?sslmode=verify-full&sslrootcert=./us-west-2-bundle.pem" : ''} npm run migrate up`,
-  (err, stdout, stderr) => {
-    console.log(`string: DATABASE_URL=postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}${process.env.NODE_ENV === "production" ? "?sslmode=verify-full&sslrootcert=./us-west-2-bundle.pem" : ''}`)
-    if (err) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      console.log(err.message);
-      return;
-    }
-    if (stderr) {
-      console.log(stderr);
-      return;
-    }
-    console.log(stdout);
+require("node-pg-migrate").runner({
+  dbClient: {
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    ssl: {
+      rejectUnauthorized: false,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-member-access
+      ca: require("fs").readFileSync("./us-west-2-bundle.pem").toString(),
+    },
   },
-);
+  dir: 'migrations',
+  direction: 'up',
+  migrationsTable: 'pgmigrations'
+})
 
 import { Controller, Get, HttpCode } from "@nestjs/common";
 
