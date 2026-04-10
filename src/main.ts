d4f8e0c547c4ff957c6697acf823eb73f3eac922
Brandon Loveall <bloveall02@gmail.com>
2026-04-09 23:23:23 -0600

    changed http to https

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ["http://localhost:4200", "http://worldofteramir.com"],
+    origin: ["http://localhost:4200", "https://worldofteramir.com"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   });
